4469a2c56ce3dd4d6fd3a71acb699e684cbb7f75
JongWoo Ha <jongwooha98@JongWoos-MacBook-Air.local>
2021-04-30 01:02:55 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import logo from './logo.png';
 import './App.css';
+import Projects from './Projects.js';
+import Articles from './Articles.js';
+import About from './About.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+        <div className="navigation">
+          <img src={logo} className="logo" alt="Logo Image" />
+          <div className="navigation-sub">
+            <button type="button" class="btn btn-outline-primary">
+              <Link to="/" className="item">Projects</Link>
+            </button>
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+          </div>
+        </div>
+      </div>
+  </BrowserRouter>
   );
 }
-
+ 
 export default App;
