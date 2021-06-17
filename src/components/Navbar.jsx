a62b38c98b63125eca3e3fe356a2d28668509bd7
JongWoo Ha <jongwooha98@gmail.com>
2021-06-17 17:33:24 +0900

    restructure navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,97 @@
-import React from 'react';
-import './Navbar.scss';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
 
+import './Navbar.scss';
+// Scroll to top of page(section) when navigating
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 function Navbar() {
+  const handleCollapse = () => {
+    document.getElementById('navbarNav').classList.remove('show');
+    document.getElementById('navbarBtn').classList.add('collapsed');
+    document.body.classList.toggle('lock-scroll');
+  };
+
+  function lockScroll() {
+    document.body.classList.toggle('lock-scroll');
+  }
   return (
     <nav className="navbar navbar-expand-lg sticky-top navbar-light">
+      <ScrollToTop />
+
       <div className="container-fluid">
-        <a className="navbar-brand" href="#home-section">
+        <Link className="navbar-brand" to="/">
           JongWoo Ha
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
+          id="navbarBtn"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarToggler"
-          aria-controls="navbarToggler"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          onClick={lockScroll}
         >
           <span className="navbar-toggler-icon" />
         </button>
 
         <div
           className="collapse navbar-collapse justify-content-end"
-          id="navbarToggler"
+          id="navbarNav"
         >
           <ul className="navbar-nav mr-auto ">
             <li className="nav-item active">
-              <a className="nav-link" href="#home-section">
+              <Link className="nav-link" to="/" onClick={handleCollapse}>
                 Home <span className="sr-only">(current)</span>
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#skills-section">
-                Skills
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#experience-section">
-                Experience
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#education-section">
+              <Link
+                className="nav-link"
+                to="/about#education"
+                onClick={handleCollapse}
+              >
                 Education
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#projects-section">
+              <Link
+                className="nav-link"
+                to="/about#experience"
+                onClick={handleCollapse}
+              >
+                Experience
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/skills" onClick={handleCollapse}>
+                Skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/projects"
+                onClick={handleCollapse}
+              >
                 Projects
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#about-section">
+              <Link className="nav-link" to="/contact" onClick={handleCollapse}>
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
