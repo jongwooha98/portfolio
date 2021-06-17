2193a6ad185a5db94ac31cc0efd33dc22326d577
JongWoo Ha <jongwooha98@gmail.com>
2021-06-17 17:34:20 +0900

    npm react-router-hash-link

--- server/config.js
+++ server/config.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
   auth: {
     user: process.env.email,
     pass: process.env.password,
