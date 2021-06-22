818b6f0734e6d98798e800220ad637334edb8f23
JongWoo Ha <jongwooha98@gmail.com>
2021-06-23 02:25:57 +0900

    minor margin changes

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,28 +1,58 @@
 import React from 'react';
-import { Button } from '../../Button';
+import { HashLink as Link } from 'react-router-hash-link';
 import './_home.scss';
+
+const STYLES = ['btn--primary', 'btn--outline'];
+const SIZES = ['btn--medium', 'btn--large'];
+
+const Button = ({ children, onClick, buttonStyle, buttonSize, linkTo }) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+  return (
+    <Link to={linkTo} className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type="button"
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
 
 function Home() {
   return (
     <div>
-      <div className="hero-container">
+      <div className="home-section">
         <h1>JONGWOO HA</h1>
-        <p>Front End Developer</p>
+        <h2>Front End Developer</h2>
         <div className="hero-btns">
+          <Button
+            linkTo="/projects"
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Explore Projects
+          </Button>
           <Button
             linkTo="/contact"
             className="btns"
             buttonStyle="btn--outline"
             buttonSize="btn--large"
           >
-            CONTACT
+            Contact
           </Button>
           <Button
             className="btns"
             buttonStyle="btn--primary"
             buttonSize="btn--large"
           >
-            DOWNLOAD RESUME <i className="far fa-file-pdf" />
+            Download Resume <i className="far fa-file-pdf" />
           </Button>
         </div>
       </div>
