801f6ff8e7c97ea13200d8470a54199797e3e268
JongWoo Ha <jongwooha98@gmail.com>
2021-06-29 16:50:53 +0900

    netlify form setup

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,84 +1,16 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-// import codesandbox from '../../../assets/images/codesandbox.png';
+import React from 'react';
 import './_contact.scss';
 
 function Contact() {
-  const [state, setState] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-  });
-
-  const [result, setResult] = useState(null);
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    axios
-      .post('/send', { ...state })
-      .then(response => {
-        setResult(response.data);
-        setState({
-          name: '',
-          email: '',
-          subject: '',
-          message: '',
-        });
-      })
-      .catch(() => {
-        setResult({
-          success: false,
-          message: 'Something went wrong. Try again later',
-        });
-      });
-  };
-
-  const handleChange = event => {
-    const { id, value } = event.target;
-
-    setState({
-      ...state,
-      [id]: value,
-    });
-  };
-  // const handleChange = event => {
-  //   const field = event.target.id;
-  //   if (field === 'name') {
-  //     setName(event.target.value);
-  //   } else if (field === 'email') {
-  //     setEmail(event.target.value);
-  //   } else if (field === 'message') {
-  //     setMessage(event.target.value);
-  //   }
-  // };
-
-  // const handleSubmit = event => {
-  //   event.preventDefault();
-  //   setStatus('Sending');
-  //   axios({
-  //     method: 'POST',
-  //     url: 'http://localhost:5000/contact',
-  //     data: useState,
-  //   }).then(response => {
-  //     if (response.data.status === 'sent') {
-  //       alert('Message Sent');
-  //       setName('');
-  //       setEmail('');
-  //       setMessage('');
-  //       setStatus('Submit');
-  //     } else if (response.data.status === 'failed') {
-  //       alert('Message Failed');
-  //     }
-  //   });
-  // };
-
-  // const buttonText = { status };
-
   return (
     <div className="contact-section container-fluid">
       <div className="contact__email">
-        <form className="contact-form" method="POST" onSubmit={handleSubmit}>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          className="contact-form"
+        >
           <div className="contact-image">
             <i className="fas fa-rocket" />
           </div>
@@ -96,11 +28,10 @@ function Contact() {
             <label htmlFor="name">
               Name
               <input
+                name="name"
                 type="text"
                 id="name"
                 className="form-control"
-                value={state.name}
-                onChange={handleChange}
               />
             </label>
           </div>
@@ -108,11 +39,10 @@ function Contact() {
             <label htmlFor="email">
               Email
               <input
+                name="email"
                 type="email"
                 id="email"
                 className="form-control"
-                value={state.email}
-                onChange={handleChange}
                 required
               />
             </label>
@@ -121,10 +51,9 @@ function Contact() {
             <label htmlFor="subject">
               Subject
               <input
+                name="subject"
                 id="subject"
                 className="form-control"
-                value={state.subject}
-                onChange={handleChange}
                 required
               />
             </label>
@@ -133,26 +62,17 @@ function Contact() {
             <label htmlFor="message">
               Message
               <textarea
+                name="message"
                 id="message"
                 className="form-control"
-                value={state.message}
-                onChange={handleChange}
                 required
               />
             </label>
           </div>
           <div className="form-group">
             <button type="submit" className="submit-button">
-              {/* {buttonText} */}
               Submit
             </button>
-          </div>
-          <div>
-            {result && (
-              <p className={`${result.success ? 'success' : 'error'}`}>
-                {result.message}
-              </p>
-            )}
           </div>
         </form>
       </div>
